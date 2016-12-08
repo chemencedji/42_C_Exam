bd3ce71e08e03873f4b5d6c0af2d85f0275ad0aa
Igor Chemencedji <ichemencedji@gmail.com>
2016-12-08 18:06:17 +0000

    Brackets

--- brackets/brackets.c
+++ brackets/brackets.c
@@ -2,28 +2,22 @@
 
 static void ft_print(int a)
 {
-    int i = 0;
     char ok[] = "OK\n";
     char er[] = "Error\n";
 
     if (a == 1)
-        while(ok[i])
-            write(1, &ok[i++], 1);
+        write(1, &ok, 3);
     else
-        while(er[i])
-            write(1, &er[i++], 1);
+        write(1, &er, 6);
 
 }
 
-static void ft_check(char **str)
+static int  ft_check2(char **str, int i, char cr)
 {
-
-    int i = -1;
     int a = 0;
     int b = 0;
     int c = 0;
-
-    while ((*str)[++i] != '\0')
+    while (i >= 0 || (*str)[i] == cr)
     {
         if ((*str)[i] == 40)
             a++;
@@ -35,8 +29,60 @@ static void ft_check(char **str)
             b--;
         if ((*str)[i] == 123)
             c++;
-        if ((*str)[i] == 124)
+        if ((*str)[i] == 125)
             c--;
+        i--;
+    }
+    if (a == 0 && b == 0 && c == 0)
+        return (0);
+    else
+        return (1);
+}
+
+static void ft_check(char **str)
+{
+
+    int i = -1;
+    int j;
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+    while ((*str)[++i] != '\0')
+    {
+        if ((*str)[i] == 40)
+            a++;
+        if ((*str)[i] == 41)
+        {
+            a--;
+            if ( 1 == (ft_check2(str, i, '(')))
+                {
+                    ft_print(0);
+                    return ;
+                }
+        }
+        if ((*str)[i] == 91)
+            b++;
+        if ((*str)[i] == 93)
+        {
+            b--;
+            if ( 1 == (ft_check2(str, i, '[')))
+            {   
+                ft_print(0);
+                return ;
+            }
+        }
+        if ((*str)[i] == 123)
+            c++;
+        if ((*str)[i] == 125)
+        {
+            c--;
+            if ( 1 == (ft_check2(str, i, '{')))
+            {   
+                ft_print(0);
+                return ;
+            }
+        }
     }
     if (a == 0 && b == 0 && c == 0)
         ft_print(1);
@@ -51,7 +97,7 @@ int main(int argc, char **argv)
     
     while (argc > ++i)
         ft_check(&argv[j++]);
-    if (j == 0)
+    if (j == 1)
         write(1, "\n", 1);
     return (1);
 }
