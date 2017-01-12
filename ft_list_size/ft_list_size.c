ffa40973ff68af5faea5663dfdb414a30dcb9acf
Igor Chemencedji <ichemencedji@gmail.com>
2017-01-12 14:33:30 +0000

    list_size

--- ft_list_size/ft_list_size.c
+++ ft_list_size/ft_list_size.c
@@ -11,9 +11,31 @@ int ft_list_size(t_list *begin_list)
     while(tmp)
     {
         tmp = tmp->next;
-        i++
+        i++;
     }
     return (i);
+}
+
+t_list *ft_create_list(t_list *begin_list)
+{
+    t_list *tmp;
+    
+    tmp = NULL;
+    if (begin_list->next == NULL)
+    {
+        tmp = (t_list*)malloc(sizeof(t_list));
+        begin_list->next = tmp;
+    }
+    else
+    {
+        tmp = begin_list;
+        while(tmp->next != NULL)
+        {
+            tmp = tmp->next;
+        }
+        tmp->next = (t_list*)malloc(sizeof(t_list));
+    }
+    return (begin_list);
 }
 
 int main(void)
@@ -22,10 +44,11 @@ int main(void)
     int i;
     
     begin_list = (t_list*)malloc(sizeof(t_list));
-    i = 5;
+    begin_list->next = NULL;
+    i = 4;
     while (i-- > 0)
     {
-        ft_create_list
+        begin_list = ft_create_list(begin_list);
     }
     printf("List size ==>> %d\n", ft_list_size(begin_list));
     return (0);
